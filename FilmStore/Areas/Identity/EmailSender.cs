bca9e3f7053999445b474b9205c7e605a78b63da
D0nya <dboeshko@gmail.com>
2019-08-21 15:18:38 +0300

    Added email sender and email confirmation.

--- FilmStore/Areas/Identity/EmailSender.cs
+++ FilmStore/Areas/Identity/EmailSender.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using MailKit.Net.Smtp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,26 @@ namespace FilmStore.Areas.Identity
 {
   public class EmailSender : IEmailSender
   {
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    public EmailSender()
     {
-      return Task.CompletedTask;
+    }
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+      MimeMessage emailMessage = new MimeMessage();
+
+      emailMessage.From.Add(new MailboxAddress("Администрация сайта", "info.FilmStore@gmail.com"));
+      emailMessage.To.Add(new MailboxAddress("", email));
+      emailMessage.Subject = subject;
+      emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage };
+
+      using (var client = new SmtpClient())
+      {
+        await client.ConnectAsync("smtp.gmail.com", 25, false);
+        await client.AuthenticateAsync("info.FilmStore@gmail.com", "FilmStorePassword11");
+        await client.SendAsync(emailMessage);
+
+        await client.DisconnectAsync(true);
+      }
     }
   }
 }
