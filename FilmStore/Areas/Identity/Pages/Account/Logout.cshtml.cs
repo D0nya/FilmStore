8d358d6cfd12316c4936510065d7e9a10ae6c463
D0nya <dboeshko@gmail.com>
2019-08-21 15:21:58 +0300

    Fixed minor bugs

--- FilmStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ FilmStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -11,34 +11,36 @@ using Microsoft.Extensions.Logging;
 
 namespace FilmStore.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class LogoutModel : PageModel
+  [AllowAnonymous]
+  public class LogoutModel : PageModel
+  {
+    private readonly SignInManager<User> _signInManager;
+    private readonly ILogger<LogoutModel> _logger;
+
+    public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
     {
-        private readonly SignInManager<User> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
-
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
-        {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return Page();
-            }
-        }
+      _signInManager = signInManager;
+      _logger = logger;
     }
+
+    public async Task OnGet()
+    {
+      await _signInManager.SignOutAsync();
+      _logger.LogInformation("User logged out.");
+    }
+
+    public async Task<IActionResult> OnPost(string returnUrl = null)
+    {
+      await _signInManager.SignOutAsync();
+      _logger.LogInformation("User logged out.");
+      if (returnUrl != null)
+      {
+        return LocalRedirect(returnUrl);
+      }
+      else
+      {
+        return Page();
+      }
+    }
+  }
 }
