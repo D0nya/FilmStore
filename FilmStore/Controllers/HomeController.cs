6bd870194494bc8569ed36c8ffcf874fa53ee96c
D0nya <dboeshko@gmail.com>
2019-08-21 15:23:08 +0300

    Added Login and Logout buttons and actions

--- FilmStore/Controllers/HomeController.cs
+++ FilmStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using FilmStore.Models;
 using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace FilmStore.Controllers
 {
@@ -11,51 +12,40 @@ namespace FilmStore.Controllers
   {
     readonly FilmStoreContext db;
     readonly IEnumerable<Film> films;
-    readonly IEnumerable<Producer> producers;
-    readonly IEnumerable<Purchase> purchases;
     public HomeController(FilmStoreContext context)
     {
       db = context;
       films = db.Films.OrderBy(film => film.Name);
-      producers = db.Producers.OrderBy(prod => prod.Name);
-      purchases = db.Purchases.Where(p => p.Customer.Id == 1);
     }
 
+    public IActionResult Privacy()
+    {
+      return View();
+    }
     public IActionResult Index()
     {
       return View(films.ToList());
     }
+
+    public IActionResult Login()
+    {
+      return Redirect("~/Identity/Account/Login");
+    }
+    public IActionResult LogOut()
+    {
+      if (HttpContext.User.Identity.IsAuthenticated)
+      {
+        return Redirect("~/Identity/Account/Logout");
+      } else {
+        return RedirectToAction("Index", "Home");
+      }
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult Purchases()
-    {
-      return View(purchases.ToList());
-    }
-
-    [HttpGet("/AddFilm")]
-    public IActionResult AddFilm()
-    {
-      ViewBag.Producers = producers;
-      return View();
-    }
-
-    [HttpPost("/AddFilm")]
-    public string AddFilm(Film film)
-    {
-      try
-      {
-        db.Films.Add(film);
-        db.SaveChanges();
-      }
-      catch(Exception ex)
-      {
-        return ex.Message;
-      }
-      return "Success!";
-    }
   }
 }
