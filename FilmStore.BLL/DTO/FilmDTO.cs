cb1bf02cf09aeca0330d54adb411f1943897126c
D0nya <dboeshko@gmail.com>
2019-09-03 09:52:59 +0300

    Added ProducerId, GenresId and CountriesId props

--- FilmStore.BLL/DTO/FilmDTO.cs
+++ FilmStore.BLL/DTO/FilmDTO.cs
@@ -9,8 +9,13 @@ namespace FilmStore.BLL.DTO
     public string Year { get; set; }
     public decimal Price { get; set; }
     public float Rate { get; set; }
+    public int ProducerId { get; set; }
     public ProducerDTO Producer { get; set; }
+
+    public List<int> CountriesId { get; set; }
     public List<CountryDTO> Countries { get; set; }
+
+    public List<int> GenresId { get; set; }
     public List<GenreDTO> Genres { get; set; }
     public List<PurchaseDTO> Purchases { get; set; }
   }
--- FilmStore.WEB/Models/FilmViewModel.cs
+++ FilmStore.WEB/Models/FilmViewModel.cs
@@ -1,17 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmStore.WEB.Models
 {
   public class FilmViewModel
   {
+    [HiddenInput(DisplayValue = false)]
     public int Id { get; set; }
+    [Display(Name = "Name")]
     public string Name { get; set; }
+    [Display(Name = "Year")]
     public string Year { get; set; }
+    [Display(Name = "Price")]
     public decimal Price { get; set; }
     public float Rate { get; set; }
+
+    [HiddenInput(DisplayValue = false)]
+    public int ProducerId { get; set; }
+
     public ProducerViewModel Producer { get; set; }
+
+    [HiddenInput(DisplayValue = false)]
+    public List<int> CountriesId { get; set; }
+
     public List<CountryViewModel> Countries { get; set; }
+
+    [HiddenInput(DisplayValue = false)]
+    public List<int> GenresId { get; set; }
+
     public List<GenreViewModel> Genres { get; set; }
+
+    [HiddenInput(DisplayValue = false)]
     public List<PurchaseViewModel> Purchases { get; set; }
   }
 }
